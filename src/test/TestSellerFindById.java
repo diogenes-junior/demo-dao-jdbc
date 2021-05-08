5e10ffef14ce78875c120e2735bf3076acfd7b62
Diogenes <diogenes.junior@gmail.com>
2021-05-08 08:35:17 -0300

    findById implementation

---
+++ src/test/TestSellerFindById.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.util.Scanner;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class TestSellerFindById {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller;
+		
+		System.out.println("Seller id: ");
+		int sellerId = input.nextInt();
+		
+		seller = sellerDao.findById(sellerId);
+		
+		System.out.println(seller);
+	}
+
+}
