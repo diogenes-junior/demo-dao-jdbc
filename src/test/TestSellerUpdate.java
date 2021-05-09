f0b42df6849ae4650dbc19d3854b103fd7f9e6c5
Diogenes <diogenes.junior@gmail.com>
2021-05-09 10:19:04 -0300

    update implementation

---
+++ src/test/TestSellerUpdate.java
@@ -0,0 +1,20 @@
+package test;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class TestSellerUpdate {
+
+	public static void main(String[] args) {
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		System.out.println("UPDATE NAME FROM Diógenes TO Diógenes da Silva Júnior");
+		Seller seller = sellerDao.findById(9);
+		seller.setName("Diógenes da Silva Júnior");
+		sellerDao.update(seller);
+		
+		System.out.println("Seller: " + seller);	
+	}
+
+}
