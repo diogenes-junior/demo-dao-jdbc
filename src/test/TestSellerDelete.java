72298e238483897020cf29ea3537b5152a4699fa
Diogenes <diogenes.junior@gmail.com>
2021-05-09 10:36:33 -0300

    delete implementation

---
+++ src/test/TestSellerDelete.java
@@ -0,0 +1,17 @@
+package test;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+
+public class TestSellerDelete {
+
+	public static void main(String[] args) {
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		//deletar vendedor codigo 9 (Diogenes)
+		sellerDao.deleteById(9);
+		System.out.println("Seller deleted.");
+
+	}
+
+}
